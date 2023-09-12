0281468e4bc5d8e6dd7d23e00df57eb83227255c
keenlight96 <nguyenkimlong29196@gmail.com>
2023-09-13 02:00:02 +0700

    finished

--- src/App.js
+++ src/App.js
@@ -8,20 +8,41 @@ import Register from "./pages/register/Register";
 import RegisterCCDV from "./pages/register/RegisterCCDV";
 import RegisterProfile from "./pages/register/RegisterProfile";
 import Home from "./pages/home/Home";
-import CCDVUserProfile from "./pages/home/CCDVUserProfile";
+import UserInfo from "./pages/information/UserInfo";
+import AllBillByOfCCDV from "./pages/information/AllBillOfCCDV";
+import Bills from "./pages/information/Bills";
+import HistoryProvider from "./pages/home/HistoryProvider";
+import ShowImages from "./pages/profile/ShowImages";
+import SidebarInformation from "./components/common/SidebarInformation";
+import Information from "./pages/information/Information";
 
 function App() {
     return (
         <>
             <Routes>
+                <Route path={"/test"} element={<SidebarInformation/>}></Route>
                 <Route path={"/login"} element={<Login/>}></Route>
                 <Route path={"/"} element={<Layout/>}>
                     <Route path={""} element={<Home/>}></Route>
                     <Route path={"register"} element={<Register/>}></Route>
-                    <Route path={"registerCCDV/:id"} element={<RegisterCCDV/>}></Route>
+                    <Route path={"registerCCDV"} element={<RegisterProfile/>}></Route>
                     <Route path={"registerProfile"} element={<RegisterProfile/>}></Route>
-                    <Route path={"/userDetail/:id"} element={<Detail/>}></Route>
-                    <Route path={"/ccdv"} element={<CCDVUserProfile/>}></Route>
+                    <Route path={"/bills"} element={<Bills/>}></Route>
+
+                    {/*Page Profile*/}
+                    <Route path={"profile/:username"} element={<Detail/>}></Route>
+
+                    {/*Page Information*/}
+                    <Route path={"information"} element={<Information/>}>
+                        <Route path={"info"} element={<UserInfo/>}></Route>
+                        <Route path={"bills"} element={<UserInfo/>}></Route>
+                        <Route path={"topup"} element={<UserInfo/>}></Route>
+                        <Route path={"summary"} element={<UserInfo/>}></Route>
+                        <Route path={"supplies"} element={<UserInfo/>}></Route>
+                        <Route path={"album"} element={<UserInfo/>}></Route>
+
+                    </Route>
+
                 </Route>
             </Routes>
         </>
